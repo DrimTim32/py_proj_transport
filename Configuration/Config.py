1cce3f6e56f4f113a6fa6c6c440d24ff7673d4cd
Marcin Malinowski <MaLiN2223@users.noreply.github.com>
2017-01-08 18:03:51 +0100

    Malin (#9)
    * basic files + config proposition
    
    * further propositions
    
    * project restructuring
    simple graph api
    
    * travist tests
    
    * working graph creating + djikstra algorithm
    added tests
    
    * refractoring and more tests
    
    * refractoring and documentation
    
    * sphinx docs by apidoc (#6)
    
    * Update README.md
    
    * compability with 2.7
    
    * sphinx docs by apidoc (#6)
    
    * Update README.md
    
    * Ania (#8)
    
    * adding branch
    
    * Add config.json
    
    * small configuration introduction + changes for different test for python 2 and 3

--- Configuration/Config.py
+++ Configuration/Config.py
@@ -3,3 +3,13 @@ class Config:
     @staticmethod
     def from_config_file(file):
         pass
+
+    @property
+    def graph_dict(self):
+        """
+        :return: returns graph description
+        :rtype: dict[str,List[tuple[str,int]]]
+        """
+        return {
+
+        }
--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup, find_packages
 
-import unittest
-
-def my_test_suite():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
-
 with open("requirements-tests.txt", 'r') as f:
     test_requirements = f.read().split('\n')
+
 with open("requirements.txt", 'r') as f:
     requirements = f.read().split('\n')
 
@@ -21,7 +15,6 @@ setup(
     author='gese anna, sobol bartek, malinowski marcin',
     author_email='',
     description='',
-    test_suite='setup.my_test_suite',
     setup_requires=requirements,
     tests_require=test_requirements
 )
