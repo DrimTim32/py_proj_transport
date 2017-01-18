b684922335af5993be1d056b8e9d87483bff7bf0
Bartek <barteks95@gmail.com>
2017-01-18 20:31:09 +0100

    class line

--- core/simulation/line.py
+++ core/simulation/line.py
@@ -1,12 +1,17 @@
+from collections import namedtuple
+
+LineStop = namedtuple('LineStop', ['name', 'time_to_next'])
+
+
 class Line:
-    def __init__(self, number, bus_capacity, frequency1, frequency2):
+    def __init__(self, number, bus_capacity, frequency1, frequency2, route1, route2):
         self.number = number
         self.bus_capacity = bus_capacity
-        self.frequencies = [1, 1]
-        self.routes = [[], []]
+        self.frequencies = [frequency1, frequency2]
+        self.routes = [route1, route2]
 
     def get_first_stop(self, route):
-        return self.routes[route][0]
+        return self.routes[route][0].name
 
     def get_last_stop(self, route):
-        return self.routes[route][-1]
+        return self.routes[route][-1].name
