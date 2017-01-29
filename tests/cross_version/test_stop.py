4e1cfcc0997b5165e31445b4f7e8612c52487773
MaLiN2223 <szyszka45@gmail.com>
2017-01-29 17:16:38 +0100

    pep8 fixes

--- tests/cross_version/test_stop.py
+++ tests/cross_version/test_stop.py
@@ -1,33 +1,38 @@
+"""
+This file contains tests for Stop class
+"""
+import sys
 import pytest
 from core.simulation.passenger_group import PassengersGroup
+from core.simulation.stop import Stop
 from tests_utils.helpers import get_full_class_name
-import sys
 
 if sys.version_info[0] >= 3:
     import unittest.mock as mock
+    from unittest.mock import PropertyMock
+
 else:
     import mock
-
-from mock import PropertyMock, MagicMock
-
-from core.simulation.stop import Stop
+    from mock import PropertyMock
 
 
 def test_create():
-    s = Stop("name")
-    assert s.name == 'name'
+    """Tests if stop is created correctly"""
+    stop = Stop("name")
+    assert stop.name == 'name'
     for i in range(10):
-        assert s.count(str(i)) == 0
+        assert stop.count(str(i)) == 0
 
 
 @pytest.mark.parametrize("dest", ["A", "B", "C"])
 @pytest.mark.parametrize("count", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 def test_coun_one_group(dest, count):
-    s = Stop("")
+    """Tests if count is being calculated correctly"""
+    stop = Stop("")
     name = get_full_class_name(PassengersGroup)
     with mock.patch(name + ".destination", new_callable=PropertyMock) as mocked_destination:
         mocked_destination.return_value = dest
         group = PassengersGroup("a", 0)
         group.count = count
-        s.passengers.append(group)
-        assert s.count(dest) == count
+        stop.passengers.append(group)
+        assert stop.count(dest) == count
