c3f536ba1973eb28175e737fb0f478f86646c603
Bartek <barteks95@gmail.com>
2017-02-02 19:47:26 +0100

    demo part

---
+++ demo/fiction/main.py
@@ -0,0 +1,30 @@
+import sys
+import time
+
+from core.configuration import config
+from core.simulation.simulation import Simulation
+
+
+def read_configuration():
+    return config.Config.from_config_file("config.json")
+
+
+def entrypoint():
+    configuration = read_configuration()
+    simulation = Simulation(configuration)
+    while True:
+        simulation.refresh()
+        time.sleep(0.2)
+
+
+def main():
+    entrypoint()
+    # try:
+    #     entrypoint()
+    # except Exception as e:
+    #     print("{0}, message : {1}".format(sys.stderr, e))
+    #     return 2
+
+
+if __name__ == "__main__":
+    sys.exit(main())
