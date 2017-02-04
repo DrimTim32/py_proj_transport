49fe1e94f4765e9b47bf2651ada660c39e73bc2d
MaLiN2223 <szyszka45@gmail.com>
2017-02-04 15:54:19 +0100

    submodule+pylint+code_check update

--- develop_scripts/submodule/updatesubmodule.bat
+++ develop_scripts/submodule/updatesubmodule.bat
@@ -2,7 +2,7 @@
 cd ../..
 echo Updating git submodule...
 git submodule init
-git submodule update
+git submodule foreach git pull origin master
 cd develop_scripts/submodule/
 echo Executing copying script.. 
 PowerShell.exe -file UpdateSubMOdule.ps1
