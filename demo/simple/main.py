f6723d8cd436e809a293325e112ed3d044781298
Anna Gese <anna.gese@gmail.com>
2017-02-07 22:47:13 +0100

    update to simple demo

--- demo/simple/main.py
+++ demo/simple/main.py
@@ -11,6 +11,7 @@ if "core" not in sys.path[0]:
 from configuration import config
 from simulation.simulation import Simulation
 
+from drawing.gui import GUI
 
 def read_configuration():
     return config.Config.from_config_file("config.json")
@@ -19,17 +20,18 @@ def read_configuration():
 def entrypoint():
     configuration = read_configuration()
     simulation = Simulation(configuration)
-    while True:
-        simulation.refresh()
-        time.sleep(0.2)
+    GUI(simulation).run()
+    exit()
 
 
 def main():
-    try:
-        entrypoint()
-    except Exception as e:
-        print("{0}, message : {1}".format(sys.stderr, e))
-        return 2
+    entrypoint()
+    
+    # try:
+    #     entrypoint()
+    # except Exception as e:
+    #     print("{0}, message : {1}".format(sys.stderr, e))
+    #     return 2
 
 
 if __name__ == "__main__":
